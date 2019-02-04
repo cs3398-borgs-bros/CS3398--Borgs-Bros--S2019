4b9a37beb458acda3f548b84d49872b41d63bc53
CarterK33 <C_k133@txstate.edu>
2019-02-04 16:48:13 -0600

    Update interface.java

--- A3/interface.java
+++ A3/interface.java
@@ -6,7 +6,9 @@ The single responsiblity principle applies to this because both the IWorkable an
 single responsiblity or function. 
 
 
-The interface segregation principle applies to the IWorker interface by splittin it up into seperate interfaces IWorkable and Ifeedable. This is more benefical because the original interface was polluted because of the two different types of workers and there different needs, the robot class doesnt need the eat function. Thus by using the interface segregation principle solves this. 
+The interface segregation principle applies to the IWorker interface by splittin it up into seperate interfaces IWorkable and Ifeedable. 
+This is more benefical because the original interface was polluted because of the two different types of workers and there different needs,
+the robot class doesnt need the eat function. Thus by using the interface segregation principle solves this. 
 
 */
 
